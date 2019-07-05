914d82c970b52559f41f7243acdc34a0b017b12c
Robin Malhotra <me@rmalhotra.com>
2019-07-06 02:26:20 +0530

    Add new user credentials

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -5,7 +5,7 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
 
   def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
+    changeset = Accounts.change_registration(%User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
@@ -20,14 +20,10 @@ defmodule RumblWeb.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
-      {:ok, user} ->
-        conn
-        |> put_flash(:info, "#{user.name} created!")
-        |> redirect(to: Routes.user_path(conn, :index))
+    user = Accounts.register_user(user_params)
 
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: Routes.user_path(conn, :index))
   end
 end
